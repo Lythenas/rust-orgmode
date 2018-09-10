4991ab43a046ef9f85bc1cce0084efec2357a7a9
Matthias Seiffert <lythenas@hotmail.de>
2018-09-10 13:17:15 +0200

    Refactored procedural macros and added a litte documentation

--- rust-orgmode-derive/src/lib.rs
+++ rust-orgmode-derive/src/lib.rs
@@ -5,15 +5,38 @@ extern crate syn;
 #[macro_use]
 extern crate quote;
 
-use proc_macro2::TokenStream;
-use syn::{DeriveInput, Data, Fields, Type};
+use proc_macro2::{TokenStream, Span};
+use syn::{DeriveInput, Data, Fields, Field, Type, TypePath};
+use syn::spanned::Spanned;
 
+/// Searched for the given field on the data.
+///
+/// Panics if `data` is not `Data::Struct` or there is no field named `field_name` in the struct.
+/// Panic messages will include the `trait_name`.
+fn get_field<'a>(trait_name: &str, field_name: &str, data: &'a Data) -> &'a Field {
+    match *data {
+        Data::Struct(ref data) => {
+            match data.fields {
+                Fields::Named(ref fields) => {
+                    fields.named.iter()
+                        .find(|ref field| field.ident.as_ref().unwrap() == field_name)
+                        .expect(&format!("{} needs a field named \"{}\".", trait_name, field_name))
+                },
+                _ => panic!("{} can only be derived on a struct with named fields.", trait_name)
+            }
+        },
+        _ => panic!("{} can only be derived on a struct.", trait_name)
+    }
+}
+
+/// Implements [`SharedBehavior`].
+///
+/// This is not derivable because it's not very useful on it's own.
 fn impl_shared_behavior(input: &DeriveInput) -> TokenStream {
-    check_shared_behavior(&input.data);
-
     let name = &input.ident;
+    let field = get_field("SharedBehavior", "shared_behavior_data", &input.data);
 
-    quote! {
+    quote_spanned! { field.span()=>
         impl SharedBehavior for #name {
             fn shared_behavior_data(&self) -> &SharedBehaviorData {
                 &self.shared_behavior_data
@@ -22,28 +45,9 @@ fn impl_shared_behavior(input: &DeriveInput) -> TokenStream {
     }
 }
 
-fn check_shared_behavior(data: &Data) {
-    let data = match data {
-        Data::Struct(data) => data,
-        _ => panic!("SharedBehavior can only be derived for a struct."),
-    };
-    let fields = match &data.fields {
-        Fields::Named(fields) => &fields.named,
-        _ => panic!("SharedBehavior can only be derived for a struct with named fields."),
-    };
-
-    let field = fields.iter().find(|ref field| field.ident.as_ref().unwrap() == "shared_behavior_data");
-    let field = match field {
-        None => panic!("SharedBehavior can only be derived for structs that have a field called \"shared_behavior_data\"."),
-        Some(field) => field,
-    };
-
-    let ty = match &field.ty {
-        Type::Path(ref ty) => ty,
-        _ => panic!(),
-    };
-}
-
+/// Derive the impl for [`Element`].
+///
+/// Also implements [`SharedBehavior`].
 #[proc_macro_derive(Element)]
 pub fn element_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
@@ -51,6 +55,7 @@ pub fn element_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream
     proc_macro::TokenStream::from(expanded)
 }
 
+/// Implements the `Element` and `SharedBehavior` traits.
 fn impl_element(input: &DeriveInput) -> TokenStream {
     let name = &input.ident;
     let shared_behavior_impl = impl_shared_behavior(&input);
@@ -62,6 +67,7 @@ fn impl_element(input: &DeriveInput) -> TokenStream {
     }
 }
 
+/// Derive the impl for [`ContainsObjects`].
 #[proc_macro_derive(ContainsObjects)]
 pub fn contains_objects_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
@@ -69,6 +75,7 @@ pub fn contains_objects_derive(input: proc_macro::TokenStream) -> proc_macro::To
     proc_macro::TokenStream::from(expanded)
 }
 
+/// Implements the `ContainsObjects` trait.
 fn impl_contains_objects(input: &DeriveInput) -> TokenStream {
     let name = &input.ident;
 
@@ -81,6 +88,9 @@ fn impl_contains_objects(input: &DeriveInput) -> TokenStream {
     }
 }
 
+/// Derive the impl for [`GreaterElement`].
+///
+/// Also implements [`Element`] and [`SharedBehavior`].
 #[proc_macro_derive(GreaterElement)]
 pub fn greater_element_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
@@ -88,6 +98,7 @@ pub fn greater_element_derive(input: proc_macro::TokenStream) -> proc_macro::Tok
     proc_macro::TokenStream::from(expanded)
 }
 
+/// Implements the `GreaterElement`, `Element` and `SharedBehavior` traits.
 fn impl_greater_element(input: &DeriveInput) -> TokenStream {
     let name = &input.ident;
     let element_impl = impl_element(&input);
@@ -95,13 +106,13 @@ fn impl_greater_element(input: &DeriveInput) -> TokenStream {
 
     quote! {
         #element_impl
-
         #contains_objects_impl
 
         impl GreaterElement for #name {}
     }
 }
 
+/// Derive the impl for [`HasAffiliatedKeywords`].
 #[proc_macro_derive(HasAffiliatedKeywords)]
 pub fn has_affiliated_keywords_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
@@ -109,6 +120,7 @@ pub fn has_affiliated_keywords_derive(input: proc_macro::TokenStream) -> proc_ma
     proc_macro::TokenStream::from(expanded)
 }
 
+/// Implements the `HasAffiliatedKeywords` trait.
 fn impl_affiliated_keywords(input: &DeriveInput) -> TokenStream {
     let name = &input.ident;
 
@@ -125,6 +137,5 @@ fn impl_affiliated_keywords(input: &DeriveInput) -> TokenStream {
 mod tests {
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
     }
 }
