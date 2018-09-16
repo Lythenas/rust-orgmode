abdec49ed0f8afdbc82fc278806f3a392c897d03
Matthias Seiffert <lythenas@hotmail.de>
2018-09-16 15:22:57 +0200

    Moved the timestamp struct and all types used by it to a submodule

--- src/types.rs
+++ src/types.rs
@@ -1779,206 +1779,212 @@ pub mod objects {
         Verbatim(String),
     }
 
-    /// A timestamp.
-    ///
-    /// # Semantics
-    ///
-    /// Timestamps are used in [`elements::Clock`] and [`elements::Planning`] and can occur in normal text.
-    ///
-    /// They represent a date and time and can be either active or inactive. Usually inactive means
-    /// that the event is already over or represents the date the event has been dealt with.
-    ///
-    /// # Syntax
-    ///
-    /// Follows one of the patterns:
-    ///
-    /// - diary sexp: `<%%(SEXP)>`
-    /// - active: `<INNER>`
-    /// - inactive: `[INNER]`
-    /// - active range: `<INNER>--<INNER>` or `<DATE TIME-TIME REPEATERORDELAY>`
-    /// - inactive range: `[INNER]--[INNER]` or `[DATE TIME-TIME REPEATERORDELAY]`
-    ///
-    /// `SEXP` can contain any character except `>` and newline.
-    ///
-    /// `INNER` is the pattern `DATE TIME REPEATERORDERLAY`.
-    ///
-    /// `DATE` follows the pattern `YYYY-MM-DD DAYNAME`. Where `Y`, `M` and `D` are digits
-    /// (`0`-`9`). `DAYNAME` is optional and can contain any non-whitespace character except `+`,
-    /// `-`, `]`, `>`, digits and newlines. Usually it is the three letter name of the weekday.
-    ///
-    /// `TIME` follows the pattern `HH:MM`. Where `H` and `M` are digits. The first `H` can be
-    /// omitted.
-    ///
-    /// `REPEATERORDELAY` follows the pattern `MARK VALUE UNIT` where `MARK` is one of `+`, `++`,
-    /// `.+`, `-` or `--` for the repeat or delay strategy. `VALUE` is a (positive) number. `UNIT`
-    /// is one of `h`, `d`, `w`, `m` or `y`.
-    ///
-    /// There can be two `REPEATERORYEAR` in the timestamp. One as a repeater and on as a warning
-    /// delay.
-    #[derive(Object, getters)]
-    #[add_fields_for(Object)]
-    pub struct Timestamp {
-        kind: TimestampKind,
-    }
+    pub use self::timestamp::Timestamp;
 
-    impl Timestamp {
-        pub fn timestamp_start(&self) -> Option<(&Date, Option<&Time>)> {
-            use self::TimestampKind::*;
-            use self::TimestampRange::*;
+    /// Contains the [`Timestamp`][`timestamp::Timestamp`] object and all structs used by it.
+    pub mod timestamp {
+        use super::*;
 
-            match &self.kind {
-                DiarySexp(_) => None, // TODO maybe this does have a start
-                Active(TimestampData {date, time, ..}) => Some((&date, time.as_ref())),
-                Inactive(TimestampData {date, time, ..}) => Some((&date, time.as_ref())),
-                ActiveRange(TimeRange(TimestampDataWithTime {date, time, ..}, ..)) => Some((&date, Some(&time))),
-                ActiveRange(DateRange(TimestampData {date, time, ..}, ..)) => Some((&date, time.as_ref())),
-                InactiveRange(TimeRange(TimestampDataWithTime {date, time, ..}, ..)) => Some((&date, Some(&time))),
-                InactiveRange(DateRange(TimestampData {date, time, ..}, ..)) => Some((&date, time.as_ref())),
+        /// A timestamp.
+        ///
+        /// # Semantics
+        ///
+        /// Timestamps are used in [`elements::Clock`] and [`elements::Planning`] and can occur in normal text.
+        ///
+        /// They represent a date and time and can be either active or inactive. Usually inactive means
+        /// that the event is already over or represents the date the event has been dealt with.
+        ///
+        /// # Syntax
+        ///
+        /// Follows one of the patterns:
+        ///
+        /// - diary sexp: `<%%(SEXP)>`
+        /// - active: `<INNER>`
+        /// - inactive: `[INNER]`
+        /// - active range: `<INNER>--<INNER>` or `<DATE TIME-TIME REPEATERORDELAY>`
+        /// - inactive range: `[INNER]--[INNER]` or `[DATE TIME-TIME REPEATERORDELAY]`
+        ///
+        /// `SEXP` can contain any character except `>` and newline.
+        ///
+        /// `INNER` is the pattern `DATE TIME REPEATERORDERLAY`.
+        ///
+        /// `DATE` follows the pattern `YYYY-MM-DD DAYNAME`. Where `Y`, `M` and `D` are digits
+        /// (`0`-`9`). `DAYNAME` is optional and can contain any non-whitespace character except `+`,
+        /// `-`, `]`, `>`, digits and newlines. Usually it is the three letter name of the weekday.
+        ///
+        /// `TIME` follows the pattern `HH:MM`. Where `H` and `M` are digits. The first `H` can be
+        /// omitted.
+        ///
+        /// `REPEATERORDELAY` follows the pattern `MARK VALUE UNIT` where `MARK` is one of `+`, `++`,
+        /// `.+`, `-` or `--` for the repeat or delay strategy. `VALUE` is a (positive) number. `UNIT`
+        /// is one of `h`, `d`, `w`, `m` or `y`.
+        ///
+        /// There can be two `REPEATERORYEAR` in the timestamp. One as a repeater and on as a warning
+        /// delay.
+        #[derive(Object, getters)]
+        #[add_fields_for(Object)]
+        pub struct Timestamp {
+            kind: TimestampKind,
+        }
+
+        impl Timestamp {
+            pub fn timestamp_start(&self) -> Option<(&Date, Option<&Time>)> {
+                use self::TimestampKind::*;
+                use self::TimestampRange::*;
+
+                match &self.kind {
+                    DiarySexp(_) => None, // TODO maybe this does have a start
+                    Active(TimestampData {date, time, ..}) => Some((&date, time.as_ref())),
+                    Inactive(TimestampData {date, time, ..}) => Some((&date, time.as_ref())),
+                    ActiveRange(TimeRange(TimestampDataWithTime {date, time, ..}, ..)) => Some((&date, Some(&time))),
+                    ActiveRange(DateRange(TimestampData {date, time, ..}, ..)) => Some((&date, time.as_ref())),
+                    InactiveRange(TimeRange(TimestampDataWithTime {date, time, ..}, ..)) => Some((&date, Some(&time))),
+                    InactiveRange(DateRange(TimestampData {date, time, ..}, ..)) => Some((&date, time.as_ref())),
+                }
+            }
+            pub fn timestamp_end(&self) -> Option<(&Date, Option<&Time>)> {
+                use self::TimestampKind::*;
+                use self::TimestampRange::*;
+
+                match &self.kind {
+                    DiarySexp(_) => None, // TODO maybe this does have an end
+                    Active(TimestampData {date, time, ..}) => Some((&date, time.as_ref())),
+                    Inactive(TimestampData {date, time, ..}) => Some((&date, time.as_ref())),
+                    ActiveRange(TimeRange(TimestampDataWithTime {date, ..}, time)) => Some((&date, Some(&time))),
+                    ActiveRange(DateRange(_, TimestampData {date, time, ..})) => Some((&date, time.as_ref())),
+                    InactiveRange(TimeRange(TimestampDataWithTime {date, ..}, time)) => Some((&date, Some(&time))),
+                    InactiveRange(DateRange(_, TimestampData {date, time, ..})) => Some((&date, time.as_ref())),
+                }
+            }
+            pub fn repeater(&self) -> Option<&Repeater> {
+                use self::TimestampKind::*;
+                use self::TimestampRange::*;
+
+                match &self.kind {
+                    DiarySexp(_) => None, // TODO maybe this does have a repeater
+                    Active(TimestampData {repeater, ..}) => repeater.as_ref(),
+                    Inactive(TimestampData {repeater, ..}) => repeater.as_ref(),
+                    ActiveRange(TimeRange(TimestampDataWithTime {repeater, ..}, _)) => repeater.as_ref(),
+                    ActiveRange(DateRange(TimestampData {repeater, ..}, _)) => repeater.as_ref(),
+                    InactiveRange(TimeRange(TimestampDataWithTime {repeater, ..}, _)) => repeater.as_ref(),
+                    InactiveRange(DateRange(TimestampData {repeater, ..}, _)) => repeater.as_ref(),
+                }
+            }
+            pub fn warning(&self) -> Option<&Warning> {
+                use self::TimestampKind::*;
+                use self::TimestampRange::*;
+
+                match &self.kind {
+                    DiarySexp(_) => None, // TODO maybe this does have a repeater
+                    Active(TimestampData {warning, ..}) => warning.as_ref(),
+                    Inactive(TimestampData {warning, ..}) => warning.as_ref(),
+                    ActiveRange(TimeRange(TimestampDataWithTime {warning, ..}, _)) => warning.as_ref(),
+                    ActiveRange(DateRange(TimestampData {warning, ..}, _)) => warning.as_ref(),
+                    InactiveRange(TimeRange(TimestampDataWithTime {warning, ..}, _)) => warning.as_ref(),
+                    InactiveRange(DateRange(TimestampData {warning, ..}, _)) => warning.as_ref(),
+                }
             }
         }
-        pub fn timestamp_end(&self) -> Option<(&Date, Option<&Time>)> {
-            use self::TimestampKind::*;
-            use self::TimestampRange::*;
 
-            match &self.kind {
-                DiarySexp(_) => None, // TODO maybe this does have an end
-                Active(TimestampData {date, time, ..}) => Some((&date, time.as_ref())),
-                Inactive(TimestampData {date, time, ..}) => Some((&date, time.as_ref())),
-                ActiveRange(TimeRange(TimestampDataWithTime {date, ..}, time)) => Some((&date, Some(&time))),
-                ActiveRange(DateRange(_, TimestampData {date, time, ..})) => Some((&date, time.as_ref())),
-                InactiveRange(TimeRange(TimestampDataWithTime {date, ..}, time)) => Some((&date, Some(&time))),
-                InactiveRange(DateRange(_, TimestampData {date, time, ..})) => Some((&date, time.as_ref())),
-            }
+        /// The kind and date for a [`Timestamp`].
+        pub enum TimestampKind {
+            DiarySexp(String),
+            Active(TimestampData),
+            Inactive(TimestampData),
+            ActiveRange(TimestampRange),
+            InactiveRange(TimestampRange),
         }
-        pub fn repeater(&self) -> Option<&Repeater> {
-            use self::TimestampKind::*;
-            use self::TimestampRange::*;
 
-            match &self.kind {
-                DiarySexp(_) => None, // TODO maybe this does have a repeater
-                Active(TimestampData {repeater, ..}) => repeater.as_ref(),
-                Inactive(TimestampData {repeater, ..}) => repeater.as_ref(),
-                ActiveRange(TimeRange(TimestampDataWithTime {repeater, ..}, _)) => repeater.as_ref(),
-                ActiveRange(DateRange(TimestampData {repeater, ..}, _)) => repeater.as_ref(),
-                InactiveRange(TimeRange(TimestampDataWithTime {repeater, ..}, _)) => repeater.as_ref(),
-                InactiveRange(DateRange(TimestampData {repeater, ..}, _)) => repeater.as_ref(),
-            }
+        /// The data for a [`TimestampKind`] with optional [`Time`].
+        pub struct TimestampData {
+            pub date: Date,
+            pub time: Option<Time>,
+            pub repeater: Option<Repeater>,
+            pub warning: Option<Warning>,
         }
-        pub fn warning(&self) -> Option<&Warning> {
-            use self::TimestampKind::*;
-            use self::TimestampRange::*;
 
-            match &self.kind {
-                DiarySexp(_) => None, // TODO maybe this does have a repeater
-                Active(TimestampData {warning, ..}) => warning.as_ref(),
-                Inactive(TimestampData {warning, ..}) => warning.as_ref(),
-                ActiveRange(TimeRange(TimestampDataWithTime {warning, ..}, _)) => warning.as_ref(),
-                ActiveRange(DateRange(TimestampData {warning, ..}, _)) => warning.as_ref(),
-                InactiveRange(TimeRange(TimestampDataWithTime {warning, ..}, _)) => warning.as_ref(),
-                InactiveRange(DateRange(TimestampData {warning, ..}, _)) => warning.as_ref(),
-            }
+        /// A date.
+        ///
+        /// This is a wrapper around [`chrono::NaiveDate`].
+        pub struct Date(NaiveDate);
+
+        /// A time.
+        ///
+        /// This is a wrapper around [`chrono::NaiveTime`].
+        pub struct Time(NaiveTime);
+
+        /// The repeater of a timestamp.
+        ///
+        /// See [`TimestampData`] and [`TimestampDataWithTime`].
+        pub struct Repeater {
+            pub period: TimePeriod,
+            pub strategy: RepeatStrategy,
         }
+
+        /// The warning delay of a timestamp.
+        ///
+        /// See [`TimestampData`] and [`TimestampDataWithTime`].
+        pub struct Warning {
+            pub delay: TimePeriod,
+            pub strategy: WarningStrategy,
+        }
+
+        /// The time period (with unit) of a [`Repeater`] or [`Warning`].
+        pub struct TimePeriod {
+            pub value: u32,
+            pub unit: TimeUnit,
+        }
+
+        /// The strategy of a [`Repeater`].
+        pub enum RepeatStrategy {
+            /// Add the repeat duration to the task date once.
+            Cumulative,
+            /// Add the repeat duration to the task date until the date is in the
+            /// future (but at leas once).
+            CatchUp,
+            /// Add the repeat duration to the current time.
+            Restart,
+        }
+
+        /// The strategy of a [`Warning`].
+        pub enum WarningStrategy {
+            /// Warns for all (repeated) date. Represented as `-` in the org file.
+            All,
+            /// Warns only for the first date. Represented as `--` in the org file.
+            First,
+        }
+
+        /// The unit of a [`TimePeriod`].
+        pub enum TimeUnit {
+            Year,
+            Month,
+            Week,
+            Day,
+            Hour,
+        }
+
+        /// The data for a timestamp range.
+        ///
+        /// See [`TimestampKind`].
+        pub enum TimestampRange {
+            /// `<DATE TIME-TIME REPEATER-OR-DELAY>` or
+            /// `[DATE TIME-TIME REPEATER-OR-DELAY]`
+            TimeRange(TimestampDataWithTime, Time),
+            /// `<DATE TIME REPEATER-OR-DELAY>--<DATE TIME REPEATER-OR-DELAY>` or
+            /// `[DATE TIME REPEATER-OR-DELAY]--[DATE TIME REPEATER-OR-DELAY]`
+            DateRange(TimestampData, TimestampData),
+        }
+
+        /// The data for a timestamp with a time.
+        ///
+        /// See [`TimestampRange`].
+        pub struct TimestampDataWithTime {
+            pub date: Date,
+            pub time: Time,
+            pub repeater: Option<Repeater>,
+            pub warning: Option<Warning>,
+        }
+
     }
-
-    /// The kind and date for a [`Timestamp`].
-    pub enum TimestampKind {
-        DiarySexp(String),
-        Active(TimestampData),
-        Inactive(TimestampData),
-        ActiveRange(TimestampRange),
-        InactiveRange(TimestampRange),
-    }
-
-    /// The data for a [`TimestampKind`] with optional [`Time`].
-    pub struct TimestampData {
-        pub date: Date,
-        pub time: Option<Time>,
-        pub repeater: Option<Repeater>,
-        pub warning: Option<Warning>,
-    }
-
-    /// A date.
-    ///
-    /// This is a wrapper around [`chrono::NaiveDate`].
-    pub struct Date(NaiveDate);
-
-    /// A time.
-    ///
-    /// This is a wrapper around [`chrono::NaiveTime`].
-    pub struct Time(NaiveTime);
-
-    /// The repeater of a timestamp.
-    ///
-    /// See [`TimestampData`] and [`TimestampDataWithTime`].
-    pub struct Repeater {
-        pub period: TimePeriod,
-        pub strategy: RepeatStrategy,
-    }
-
-    /// The warning delay of a timestamp.
-    ///
-    /// See [`TimestampData`] and [`TimestampDataWithTime`].
-    pub struct Warning {
-        pub delay: TimePeriod,
-        pub strategy: WarningStrategy,
-    }
-
-    /// The time period (with unit) of a [`Repeater`] or [`Warning`].
-    pub struct TimePeriod {
-        pub value: u32,
-        pub unit: TimeUnit,
-    }
-
-    /// The strategy of a [`Repeater`].
-    pub enum RepeatStrategy {
-        /// Add the repeat duration to the task date once.
-        Cumulative,
-        /// Add the repeat duration to the task date until the date is in the
-        /// future (but at leas once).
-        CatchUp,
-        /// Add the repeat duration to the current time.
-        Restart,
-    }
-
-    /// The strategy of a [`Warning`].
-    pub enum WarningStrategy {
-        /// Warns for all (repeated) date. Represented as `-` in the org file.
-        All,
-        /// Warns only for the first date. Represented as `--` in the org file.
-        First,
-    }
-
-    /// The unit of a [`TimePeriod`].
-    pub enum TimeUnit {
-        Year,
-        Month,
-        Week,
-        Day,
-        Hour,
-    }
-
-    /// The data for a timestamp range.
-    ///
-    /// See [`TimestampKind`].
-    pub enum TimestampRange {
-        /// `<DATE TIME-TIME REPEATER-OR-DELAY>` or
-        /// `[DATE TIME-TIME REPEATER-OR-DELAY]`
-        TimeRange(TimestampDataWithTime, Time),
-        /// `<DATE TIME REPEATER-OR-DELAY>--<DATE TIME REPEATER-OR-DELAY>` or
-        /// `[DATE TIME REPEATER-OR-DELAY]--[DATE TIME REPEATER-OR-DELAY]`
-        DateRange(TimestampData, TimestampData),
-    }
-
-    /// The data for a timestamp with a time.
-    ///
-    /// See [`TimestampRange`].
-    pub struct TimestampDataWithTime {
-        pub date: Date,
-        pub time: Time,
-        pub repeater: Option<Repeater>,
-        pub warning: Option<Warning>,
-    }
-
-
 
 }
