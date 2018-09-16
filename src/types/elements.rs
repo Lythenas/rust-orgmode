95caa932b5554b6465fd2b08c115c27b29ce1486
Matthias Seiffert <lythenas@hotmail.de>
2018-09-16 20:35:30 +0200

    Split the types module into submodules

---
+++ src/types/elements.rs
@@ -0,0 +1,435 @@
+//! Contains all elements except [`greater_elements`].
+
+use super::*;
+use rust_orgmode_derive::add_fields_for;
+
+/// A babel call element.
+///
+/// # Sematics
+///
+/// Used to execute [`SrcBlock`]s and put their results into the org file.
+///
+/// # Syntax
+///
+/// ```text
+/// #+CALL: FUNCTION[INSIDE-HEADER](ARGUMENTS) END-HEADER
+/// ```
+///
+/// `FUNCTION` is the name of a [`SrcBlock`] to execute. `INSIDE-HEADER`, `ARGUEMENTS` and
+/// `END-HEADER` can contain everything except a newline (and their respective closing char).
+#[derive(Element, HasAffiliatedKeywords, getters)]
+#[add_fields_for(SharedBehavior, HasAffiliatedKeywords)]
+pub struct BabelCall {
+    /// The code block to call
+    call: String,
+    inside_header: String,
+    arguments: String,
+    end_header: String,
+}
+
+/// A clock element.
+///
+/// # Sematics
+///
+/// A clock element is used to track working time. When clocked in the timestamp part is only a
+/// date and time. When clocked out the timestamp part is a datetime range. And the duration is
+/// the duration of the range.
+///
+/// The timestamps are usually inactive.
+///
+/// # Syntax
+///
+/// ```text
+/// CLOCK: TIMESTAMP DURATION
+/// ```
+///
+/// `CLOCK` is the literal word `CLOCK`.
+///
+/// `TIMESTAMP` and `DURATION` are optional. `TIMESTAMP` is a [`objects::Timestamp`].
+///
+/// `DURATION` follows the pattern `=> HH:MM` where `HH` is a number containing any number of
+/// digits and `MM` is a two digit number.
+#[derive(Element, getters)]
+#[add_fields_for(SharedBehavior)]
+pub struct Clock {
+    timestamp: Option<objects::Timestamp>,
+    duration: Option<(u64, u8)>,
+}
+
+impl Clock {
+    pub fn status(&self) -> ClockStatus {
+        match self.duration {
+            Some(_) => ClockStatus::Closed,
+            None => ClockStatus::Running,
+        }
+    }
+}
+
+pub enum ClockStatus {
+    Running,
+    Closed,
+}
+
+/// A comment element.
+///
+/// # Semantics
+///
+/// Comments are ignored when parsing. They are not actually ignored, they just don't have any
+/// meaning.
+///
+/// # Snytax
+///
+/// A line starting with `#` and space (or end of line). The `#` can be optionally preceded
+/// with whitespace.
+///
+///
+/// ```text
+/// # CONTENTS
+/// ```
+///
+/// `CONTENTS` can be any string.
+///
+/// Consecutive comment lines are accumulated into one comment.
+#[derive(Element, HasAffiliatedKeywords, getters)]
+#[add_fields_for(SharedBehavior, HasAffiliatedKeywords)]
+pub struct Comment {
+    value: String,
+}
+
+/// A comment block.
+///
+/// # Semantics
+///
+/// See [`Comment`].
+///
+/// # Syntax
+///
+/// ```text
+/// #+BEGIN_COMMENT
+/// CONTENTS
+/// #+END_COMMENT
+/// ```
+///
+/// `CONTENTS` can contain anything except a line `#+END_COMMENT` on its own. Lines beginning
+/// with stars must be quoted by a comma. `CONTENTS` will not be parsed.
+#[derive(Element, HasAffiliatedKeywords, getters)]
+#[add_fields_for(SharedBehavior, HasAffiliatedKeywords)]
+pub struct CommentBlock {
+    value: String,
+}
+
+/// A diary sexp.
+///
+/// # Semantics
+///
+/// Diary sexps are special function to related to date and time. E.g. you can automatically
+/// calculate the age of someone by giving it a birthday. It can also display all holidays.
+///
+/// See <https://orgmode.org/manual/Weekly_002fdaily-agenda.html> for more info.
+///
+/// # Syntax
+///
+/// ```text
+/// %%(VALUE
+/// ```
+///
+/// `VALUE` can contain any character except a newline. The expression has to start at the
+/// beginning of the line.
+#[derive(Element, HasAffiliatedKeywords, getters)]
+#[add_fields_for(SharedBehavior, HasAffiliatedKeywords)]
+pub struct DiarySexp {
+    value: String,
+}
+
+/// An example block.
+///
+/// # Semantics
+///
+/// Its content will not be parsed. Examples are typeset in monospace when exporting.
+///
+/// # Syntax
+///
+/// ```text
+/// #+BEGIN_EXAMPLE FLAGS
+/// CONTENTS
+/// #+END_EXAMPLE
+/// ```
+///
+/// `CONTENTS` can contain anything except a line `#+END_EXAMPLE` on its own. Lines beginning
+/// with stars must be quoted by comma. `CONTENTS` will not be parsed. `CONTENT` can also
+/// contain labels with the pattern `(ref:LABEL)`. **Labels are not recognized.**
+///
+/// `FLAGS` see [`BlockFlags`].
+#[derive(Element, HasAffiliatedKeywords, getters)]
+#[add_fields_for(SharedBehavior, HasAffiliatedKeywords)]
+pub struct ExampleBlock {
+    value: String,
+    flags: BlockFlags,
+}
+
+/// Contains the flags of an [`ExampleBlock`] or [`SrcBlock`].
+///
+/// Can contain the following flags:
+///
+/// - `+n AMOUNT`: continued number lines, will continue the numbering of the previos numbered
+///   snippet. `AMOUNT` will be added to the last line of the previod block to determine the
+///   number of the first line.
+/// - `-n AMOUNT`: new number lines (`AMOUNT` is the start line number of the block)
+/// - `-i`: preserve indent
+/// - `-r`: removes the labels when exporting. References will use line numbers.
+/// - `-k`: don't use labels
+/// - `-l "FMT"`: label format (if the default format conflicts with the language you are
+///   using)
+///
+/// `AMOUNT` is an optional positive number.
+///
+/// `FMT` can contain everything except `"` and newlines.
+#[derive(getters)]
+pub struct BlockFlags {
+    number_lines: Option<NumberLinesFlag>,
+    /// Default: false
+    preserve_indent: bool,
+    /// Default: true
+    ///
+    /// If true, code-references should use labels instead of line numbers.
+    retain_labels: bool,
+    label_fmt: Option<String>,
+}
+
+pub enum NumberLinesFlag {
+    Continued(Option<u64>),
+    New(Option<u64>),
+}
+
+/// An export block.
+///
+/// # Semantics
+///
+/// TODO
+///
+/// # Syntax
+///
+/// ```text
+/// #+BEGIN_EXPORT BACKEND
+/// CONTENTS
+/// #+END_EXPORT
+/// ```
+///
+/// `CONTENTS` can contain anything except a line `#+END_EXAMPLE` on its own. Lines beginning
+/// with stars must be quoted by comma. `CONTENTS` will not be parsed.
+///
+/// `BACKEND` can contain any alpha-numerical character. Case is ignored.
+#[derive(Element, HasAffiliatedKeywords, getters)]
+#[add_fields_for(SharedBehavior, HasAffiliatedKeywords)]
+pub struct ExportBlock {
+    value: String,
+    /// Always lowercase.
+    backend: String,
+}
+
+/// A fixed width area.
+///
+/// # Semantics
+///
+/// Can be used in lists or text for examples. Similar to [`ExampleBlock`] but can be indented.
+///
+/// # Syntax
+///
+/// A line beginning with `:` followed by a whitespace or end of line. The `:` can be preceded
+/// by whitespace.
+///
+/// Consecutive fixed width lines are accumulated.
+#[derive(Element, HasAffiliatedKeywords, getters)]
+#[add_fields_for(SharedBehavior, HasAffiliatedKeywords)]
+pub struct FixedWidth {
+    value: String,
+}
+
+/// A horizontal line.
+///
+/// # Semantics
+///
+/// A horizontal line.
+///
+/// # Syntax
+///
+/// A line of at least 5 consecutive hyphens. Can be precesed by whitespace.
+///
+/// ```text
+/// -----
+/// ```
+#[derive(Element, HasAffiliatedKeywords, getters)]
+#[add_fields_for(SharedBehavior, HasAffiliatedKeywords)]
+pub struct HorizontalRule {
+}
+
+/// A keyword.
+///
+/// # Semantics
+///
+/// A keywords is similar to [`AffiliatedKeywords`] but they don't belong to another element.
+/// Orphaned affiliated keywords are considered regular keywords.
+///
+/// # Syntax
+///
+/// ```text
+/// #+KEY: VALUE
+/// ```
+///
+/// `KEY` can contain any non-whitespace character. But it can't be equal to `CALL` or any
+/// affiliated keyword.
+///
+/// `VALUE` can contain any character except a newline.
+#[derive(Element, HasAffiliatedKeywords, getters)]
+#[add_fields_for(SharedBehavior, HasAffiliatedKeywords)]
+pub struct Keyword {
+    key: String,
+    value: String,
+}
+
+/// A document property keyword.
+///
+/// # Semantics
+///
+/// See [`Keyword`] but for the whole org file.
+///
+/// # Syntax
+///
+/// See [`Keyword`].
+///
+/// `VALUE` is parsed as a [`SecondaryString`].
+#[derive(Element, HasAffiliatedKeywords, getters)]
+#[add_fields_for(SharedBehavior, HasAffiliatedKeywords)]
+pub struct DocumentProperty {
+    key: String,
+    value: SecondaryString,
+}
+
+/// A LaTeX environment.
+///
+/// # Semantics
+///
+/// This will be treated as accordingly when exporting with LaTeX. Otherwise it will be treated
+/// as plain text.
+///
+/// # Syntax
+///
+/// ```text
+/// \begin{ENVIRONMENT}
+/// CONTENTS
+/// \end{ENVIRONMENT}
+/// ```
+///
+/// `ENVIRONMENT` can contain any alpha-numeric character and asterisks. Usually the asterisk
+/// is only at the end.
+///
+/// `CONTENT` can be anything except `\end{ENVIRONMENT}`.
+#[derive(Element, HasAffiliatedKeywords, getters)]
+#[add_fields_for(SharedBehavior, HasAffiliatedKeywords)]
+pub struct LatexEnvironment {
+    /// Contains everything including `\begin...` and `\end`.
+    value: String,
+}
+
+/// A node property.
+///
+/// # Semantics
+///
+/// A property contained in a [`greater_elements::PropertyDrawer`].
+///
+/// # Syntax
+///
+/// Follows one of these patterns:
+///
+/// - `:NAME: VALUE`
+/// - `:NAME+: VALUE`
+/// - `:NAME:`
+/// - `:NAME+:`
+///
+/// `NAME` can contain any non-whitespace character but can't be an empty string or end with a
+/// plus sign (`+`).
+///
+/// `VALUE` can contain anything but a newline character.
+#[derive(Element, getters)]
+#[add_fields_for(SharedBehavior)]
+pub struct NodeProperty {
+    name: String,
+    value: String,
+}
+
+/// A paragraph.
+///
+/// # Semantics
+///
+/// A paragraph is a list of strings and objects ([`SecondaryString`]). Line breaks in the text
+/// are ignored and only [`objects::LineBreak`] will be recognized as a line break.
+///
+/// # Syntax
+///
+/// Everything that is not another element is paragraph. Empty lines and other elements end
+/// paragraphs but all inner elements of the current paragraph must be closed first.
+#[derive(Element, HasAffiliatedKeywords, getters)]
+#[add_fields_for(SharedBehavior, HasAffiliatedKeywords)]
+pub struct Paragraph {
+    // TODO figure out how to actually handle secondary strings
+}
+
+/// A planning element.
+///
+/// # Semantics
+///
+/// Contains the deadline, scheduled and closed timestamps for a headline. All are optional.
+///
+/// # Syntax
+///
+/// Planning lines are context-free.
+///
+/// ```text
+/// KEYWORD: TIMESTAMP
+/// ```
+///
+/// `KEYWORD` is one of `DEADLINE`, `SCHEDULED` or `CLOSED`. Planning can be repeated but one
+/// keywords can only be used once. The order doesn't matter.
+///
+/// `TIMESTAMP` is a [`objects::Timestamp`].
+///
+/// Consecutive planning items are aggregated into one.
+#[derive(Element, getters)]
+#[add_fields_for(SharedBehavior)]
+pub struct Planning {
+    closed: Option<objects::Timestamp>,
+    deadline: Option<objects::Timestamp>,
+    scheduled: Option<objects::Timestamp>,
+}
+
+/// A block of source code.
+///
+/// # Semantics
+///
+/// Same as [`ExampleBlock`] but usually contains source code. The content will be highlighted
+/// according to the language specified.
+///
+/// # Syntax
+///
+/// ```text
+/// #+BEGIN_SRC LANGUAGE FLAGS ARGUMENTS
+/// CONTENTS
+/// #+END_SRC
+/// ```
+///
+/// `CONTENTS` can contain anything except a line `#+END_SRC` on its own. Lines beginning
+/// with stars must be quoted by comma. `CONTENTS` will not be parsed.
+///
+/// `LANGUAGE` can contain anything except whitespace.
+///
+/// `FLAGS` see [`BlockFlags`].
+///
+/// `ARGUMENTS` can contain any character except a newline.
+#[derive(Element, HasAffiliatedKeywords, getters)]
+#[add_fields_for(SharedBehavior, HasAffiliatedKeywords)]
+pub struct SrcBlock {
+    language: String,
+    flags: BlockFlags,
+    arguments: String,
+}
+
---
+++ src/types/mod.rs
@@ -0,0 +1,282 @@
+//! This module contains all types and traits needed to represent an org file.
+//!
+//! The *elements* of a org file are separated into three categories:
+//!
+//! - [`Object`]s represent the content of the file.
+//! - [`Element`]s represent the structure of the file.
+//! - [`GreaterElement`] is a kind of [`Element`] that can contain other elements.
+//!
+//! # Implementation of shared behavior
+//!
+//! There is some shared behavior between elements an objects. This is organized into the
+//! traits: [`SharedBehavior`], [`ContainsObjects`] and [`HasAffiliatedKeywords`]. Those traits
+//! rely on specific data being stored in the elements/objects. To simplify this the data is
+//! stored in helper traits and these helper traits are then stored in elements/objects. The
+//! element/object structs only need to implement a getter method for the helper struct and the
+//! trait will give them getter methods for the data in those helper structs.
+//!
+//! [`Object`]: trait.Object.html
+//! [`Element`]: trait.Element.html
+//! [`GreaterElement`]: trait.GreaterElement.html
+//! [`SharedBehavior`]: trait.SharedBehavior.html
+//! [`ContainsObjects`]: trait.ContainsObjects.html
+//! [`HasAffiliatedKeywords`]: trait.HasAffiliatedKeywords.html
+
+use std::collections::HashMap;
+
+pub mod elements;
+pub mod greater_elements;
+pub mod objects;
+
+// TODO using lazy_static
+#[allow(dead_code)]
+static ORG_ENTITIES: () = ();
+#[allow(dead_code)]
+static ORG_LINK_TYPES: () = ();
+
+/// All greater elements, elements and objects share some shared behavior.
+///
+/// This trait adds getters for the needed properties to the elements/objects. The following
+/// properties are needed:
+///
+/// - **span**: Marks where in the document this element is located. Used for error/warning messages
+/// - **post blank**: Blank lines and whitespace at the end of the element.
+/// - **parent**: The parent element that contains this one.
+///
+/// The actual data is stored in the convenience struct [`SharedBehaviorData`]. The implementing
+/// structs only need to implement `shared_behavior_data()` and this trait will provide the
+/// getters for the fields of the `SharedBehaviorData` struct.
+///
+/// [`SharedBehaviorData`]: struct.SharedBehaviorData.html
+pub trait SharedBehavior {
+    /// Returns a reference to the data of the shared behavior.
+    ///
+    /// You should most likely not use this method. It is just a proxy for the other methods on
+    /// this trait.
+    ///
+    /// Wenn implementing this method you should simply return the field that stores this data.
+    fn shared_behavior_data(&self) -> &SharedBehaviorData;
+
+    fn span(&self) -> &Span {
+        &self.shared_behavior_data().span
+    }
+
+    fn post_blank(&self) -> &u32 {
+        &self.shared_behavior_data().post_blank
+    }
+
+    fn parent(&self) -> &Option<ParentId> {
+        &self.shared_behavior_data().parent
+    }
+}
+
+/// Helper struct that contains the data for the shared behavior. See [`SharedBehavior`].
+///
+/// [`SharedBehavior`]: trait.SharedBehavior.html
+pub struct SharedBehaviorData {
+    span: Span,
+    post_blank: u32,
+    parent: Option<ParentId>,
+}
+
+/// Represents where in the file the a object or element is.
+///
+/// It contains a start and an end. `end` is always bigger than or equal to `start`.
+///
+/// This is useful for warning/error messages and modifying the file.
+pub struct Span {
+    start: u64,
+    end: u64,
+}
+
+impl Span {
+    pub fn new(start: u64, end: u64) -> Self {
+        Span { start, end }
+    }
+
+    pub fn start(&self) -> u64 {
+        self.start
+    }
+
+    pub fn end(&self) -> u64 {
+        self.end
+    }
+}
+
+/// This represents a parent in the storage engine (TODO).
+pub struct ParentId;
+
+/// Some greater elements, elements and objects can contain other objects.
+///
+/// These elements and objects have the following additional properties:
+///
+/// - **content span**: Marks where in the document the content begins and ends.
+/// - **content**: A list of all elements, objects and raw string contained in this element or
+///   object.
+///
+/// The actual data is stored in the convenience struct [`ContentData`]. The implementing structs
+/// only need to implement `content_data()` and this trait will provide the getters for the fields
+/// of the `ContentData` struct.
+///
+/// [`ContentData`]: struct.ContentData.html
+pub trait ContainsObjects: SharedBehavior {
+    /// Returns a reference to the data needed to contain objects.
+    ///
+    /// You should most likely not use this method. It is just a proxy for the other methods on
+    /// this trait.
+    ///
+    /// Wenn implementing this method you should simply return the field that stores this data.
+    fn content_data(&self) -> &ContentData;
+
+    fn content_span(&self) -> &Span {
+        &self.content_data().span
+    }
+
+    fn content(&self) -> &Vec<ObjectId> {
+        &self.content_data().content
+    }
+}
+
+/// Helper struct that contains the data for the elements and objects that can contain other
+/// objects.
+///
+/// See [`ContainsObjects`].
+///
+/// [`ContainsObjects`]: trait.ContainsObjects.html
+pub struct ContentData {
+    span: Span,
+    content: Vec<ObjectId>,
+}
+
+/// This is an id in the storage engine (TODO).
+pub struct ObjectId;
+
+/// Some greater elements and elements can have affiliated keywords.
+///
+/// Those elements have to following additional properties:
+///
+/// - **affiliated keywords span**: Marks where in the document the affiliated keywords are
+///   location.
+/// - **affiliated keywords**: Contains all affiliated keywords for this element.
+///
+/// The actual data is stored in the convenience struct [`AffiliatedKeywordsData`]. The
+/// implementing structs only need to implement `affiliated_keywords_data()` and this trait will
+/// provide the getters for the fields of the `AffiliatedKeywordsData` struct.
+///
+/// [`AffiliatedKeywordsData`]: struct.AffiliatedKeywordsData.html
+pub trait HasAffiliatedKeywords: Element {
+    /// Returns a reference to the data needed to have affiliated keywords.
+    ///
+    /// You should most likely not use this method. It is just a proxy for the other methods on
+    /// this trait.
+    ///
+    /// Wenn implementing this method you should simply return the field that stores this data.
+    fn affiliated_keywords_data(&self) -> &AffiliatedKeywordsData;
+
+    fn affiliated_keywords(&self) -> &AffiliatedKeywords {
+        &self.affiliated_keywords_data().affiliated_keywords
+    }
+
+    fn affiliated_keywords_span(&self) -> &Span {
+        &self.affiliated_keywords_data().span
+    }
+}
+
+/// Helper struct that contains the data for the elements that have affiliated keywords.
+///
+/// See [`HasAffiliatedKeywords`].
+///
+/// [`HasAffiliatedKeywords`]: trait.HasAffiliatedKeywords.html
+pub struct AffiliatedKeywordsData {
+    affiliated_keywords: AffiliatedKeywords,
+    span: Span,
+}
+
+/// Contains all affiliated keywords for one element/object.
+///
+/// An affiliated keyword represents an attribute of an element.
+///
+/// Not all elements can have affiliated keywords. See the specific element.
+///
+/// Affiliated keywords have one of the following formats:
+///
+/// - `#+KEY: VALUE`
+/// - `#+KEY[OPTIONAL]: VALUE`
+/// - `#+ATTR_BACKEND: VALUE`
+pub struct AffiliatedKeywords {
+    /// Parsed from: `#+CAPTION[OPTIONAL]: VALUE`.
+    ///
+    /// Where `OPTIONAL` (and the brackets) are optional and both `OPTIONAL` and `VALUE` are
+    /// secondary strings (can contain objects).
+    ///
+    /// The caption key can occur more than once.
+    pub caption: Vec<SpannedValue<(Option<SecondaryString>, SecondaryString)>>,
+    /// Parsed from: `#+HEADER: VALUE`.
+    ///
+    /// The header key can occur more than once.
+    ///
+    /// The deprecated `HEADERS` key will also be parsed to this variant.
+    pub header: Vec<SpannedValue<String>>,
+    /// Parsed from: `#+NAME: VALUE`.
+    ///
+    /// The deprecated `LABEL`, `SRCNAME`, `TBLNAME`, `DATA`, `RESNAME` and `SOURCE` keys will also
+    /// be parsed to this variant.
+    pub name: Option<SpannedValue<String>>,
+    /// Parsed from: `#+PLOT: VALUE`.
+    pub plot: Option<SpannedValue<String>>,
+    /// Parsed from: `#+RESULTS[OPTIONAL]: VALUE`.
+    ///
+    /// Where `OPTIONAL` (and the brackets) are optional and both `OPTIONAL` and `VALUE` are
+    /// secondary strings (can contain objects).
+    ///
+    /// The deprecated `RESULT` key will also be parsed to this variant.
+    pub results: Option<SpannedValue<(Option<String>, String)>>,
+    /// Parsed from: `#+ATTR_BACKEND: VALUE`.
+    ///
+    /// The attr keywords for one backend can occur more than once.
+    pub attrs: HashMap<String, Vec<SpannedValue<String>>>,
+}
+
+/// Represents a value and its position in an org file.
+pub struct SpannedValue<T> {
+    span: Span,
+    value: T,
+}
+
+impl<T> SpannedValue<T> {
+    pub fn new(span: Span, value: T) -> Self {
+        SpannedValue { span, value }
+    }
+    pub fn span(&self) -> &Span {
+        &self.span
+    }
+    pub fn value(&self) -> &T {
+        &self.value
+    }
+}
+
+/// A secondary string is a list of raw strings and objects.
+///
+/// It is used for attributes of elements that can contain objects.
+pub struct SecondaryString(Vec<ObjectId>);
+
+/// Marker trait for objects in an org file.
+///
+/// Objects are the smallest units and represent the content of the org file.
+pub trait Object: SharedBehavior {}
+
+/// Marker trait for the elements in an org file.
+///
+/// Elements represent the structure of the org file.
+///
+/// See [`elements`] module for all available elements.
+pub trait Element: SharedBehavior {}
+
+/// Marker trait for the greater elements in an org file.
+///
+/// Greater elements are elements which can contain other (greater) elements. Usually they can't
+/// contain themselfes (see the specific element for more details).
+///
+/// See [`greater_elements`] module for all available greater elements.
+pub trait GreaterElement: Element + ContainsObjects {}
+
